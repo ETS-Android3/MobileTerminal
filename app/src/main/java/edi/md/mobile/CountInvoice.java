3ec0aaa7deae542ed65d0b303720eccc19ac4e7b
cristea-igor <koolpix404@gmail.com>
2019-08-02 13:46:57 +0300

    countinvoice1

--- app/src/main/java/edi/md/mobile/CountInvoice.java
+++ app/src/main/java/edi/md/mobile/CountInvoice.java
@@ -26,7 +26,7 @@ import org.json.JSONObject;
 
 public class CountInvoice extends AppCompatActivity {
 //gfudszxcvliguiljknbhvku6t7yh
-    //kjhguykuyhjbhvkygu
+    //kjhhjbhvkygu
 // h
     Button btn_accept,btn_cancel;
     TextView txtTotalinc,txtTotalsale,txtNames,txtProfit;
